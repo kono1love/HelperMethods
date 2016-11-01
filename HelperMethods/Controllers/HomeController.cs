3d92ad948fee1d922d1545ed761c9e8adb2e7442
Vladislav <Vladislav@KONO1LOVE>
2016-11-01 10:52:05 +0300

    Done chapter 21. Create project, use  different helpers methods

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using HelperMethods.Models;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+
+            string message = "This is an HTML element: <input>";
+            return View((object)message);
+        }
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+        [HttpPost]
+        public ActionResult CreatePerson(Person person)
+        {
+            return View(person);
+        }
+    }
+}
